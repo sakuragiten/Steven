ff56ed2942e116d70182163324bc40f460a8474c
gongsheng <gongs@dachentech.com.cn>
2018-09-20 15:20:23 +0800

    Test

---
+++ StevenNash/StevenNash/Classes/Progress/CircleProgressView.swift
@@ -0,0 +1,48 @@
+//
+//  CircleProgressView.swift
+//  StevenNash
+//
+//  Created by gongsheng on 2018/9/20.
+//  Copyright © 2018 gongsheng. All rights reserved.
+//
+
+import UIKit
+
+class CircleProgressView: UIView {
+    
+    //进度
+    var progress : CGFloat = 0.0 {
+        didSet {
+            print("test")
+        }
+    }
+    
+    //进度条的颜色
+    var progressColor : UIColor = .red
+    
+    var progressLayer : CAShapeLayer?
+    
+    
+    
+    override func draw(_ rect: CGRect) {
+        let path = circlrPathWithProgress(1.0)
+        path.lineWidth = 1.0
+        UIColor.lightGray.setStroke()
+        path.stroke()
+    }
+    
+    private func circlrPathWithProgress(_ progress : Double) -> UIBezierPath {
+        
+        let original = CGPoint(x: self.bounds.size.width * 0.5, y: self.bounds.size.height * 0.5)
+        
+        let radius = self.bounds.size.width * 0.5 - 2.0
+        let startAngle = -(Double.pi / 2.0)
+        let endAngle = startAngle + progress * Double.pi * 2.0
+        
+        let path = UIBezierPath(arcCenter: original, radius: radius, startAngle: CGFloat(startAngle), endAngle: CGFloat(endAngle), clockwise: true)
+        
+        
+        return path
+    }
+    
+}
